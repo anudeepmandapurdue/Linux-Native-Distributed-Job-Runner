93a2c8978bddcbcf1cdb7cbf442172f2582a2020
anudeepmandapurdue <anudeepsm13@gmail.com>
2026-01-26 22:09:20 -0500

    Update main.c

--- c-worker/main/main.c
+++ c-worker/main/main.c
@@ -15,6 +15,21 @@ int main(){
         perror("pipe");
         exit(1);
     }
+    // FORK: clone the parent process
+    pid = fork();
+    
+    if(pid == 0){ //child process
+        //conect the stdout to write end of the pipi
+        dup2(pipefd[1], STDOUT_FILENO); //dup2 function: duplicate the file descriptor 2. takes the write end of the pipe and forces it to be stdout
+        close(pipefd[0]);
+        
 
+        //EXEC: replace child node with 'ls'
+        char *args[] = {"ls", "-la", NULL};
+        execvp(args[0], args);
 
+    }
+    else { //parent process
+        close(pipefd[1]);
+    }
 }
